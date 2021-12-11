fd8bd9813a059db9511feac56360b3a124206df8
Imtiaz Adar <75721079+imtiazaadar@users.noreply.github.com>
2021-12-11 18:19:45 +0600

    SNAKE GAME BY IMTIAZ ADAR

---
+++ Snake Game/src/snakegame/SnakeGame.java
@@ -0,0 +1,27 @@
+package snakegame;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+/*
+ * Imtiaz Adar
+ * Email : imtiaz-adar@hotmail.com
+ * Project : SNAKE GAME
+ * Language Used : Java
+ */
+public class SnakeGame extends JFrame {
+	private ImageIcon icon;
+	public SnakeGame() {
+		icon = new ImageIcon(getClass().getResource("snakepic.png"));
+		this.setIconImage(icon.getImage());
+		this.add(new SnakeGamePanel());
+		this.setBounds(100, 30, 1300, 750);
+		this.setTitle("SNAKE GAME BY IMTIAZ ADAR");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLayout(null);
+	}
+	public static void main(String[] args) {
+		SnakeGame snake = new SnakeGame();
+	}
+}
